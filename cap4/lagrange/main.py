8179eda3a16df6b1c4e39d4e6d96d6768afa0e5b
phenrique <ph.cardoso10@gmail.com>
2020-12-09 14:13:27 -0300

    adicionado método de Lagrange

---
+++ cap4/lagrange/main.py
@@ -0,0 +1,14 @@
+#import matplotlib as plt
+import numpy as np
+
+#x = [0.9, 1, 1.1]
+#y = [0.6216, 0.5403, 0.4536]
+
+x = [1, 3]
+y = [2, 1]
+
+P = []
+
+for i in range(0, len(x)):
+    pod *= 
+    P.append()
